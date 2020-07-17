90735b3244d2167e3434727ac12d46551655cdb5
CarbonicKevin <kevin.kimminjun@gmail.com>
2020-07-17 01:36:36 -0400

    Colab didn't save stuff so recoding

--- model/lib/SemiSup.py
+++ model/lib/SemiSup.py
@@ -5,24 +5,24 @@ class semiSupA(nn.Module):
         super(semiSupA, self).__init__()
         self.name = name
         self.conv = nn.Sequential( # like the Composition layer you built
-            nn.Conv2d(10, 13, 3),
+            nn.Conv2d(15, 20, 3),
             nn.MaxPool2d(2, stride=2),
             nn.ReLU(),
-            nn.Conv2d(13, 16, 4),
+            nn.Conv2d(20, 25, 4),
             nn.MaxPool2d(2, stride=2),
             nn.ReLU(),
-            nn.Conv2d(16, 20, 4),
+            nn.Conv2d(25, 30, 4),
             nn.MaxPool2d(2, stride=2),
             nn.ReLU()
         )
         self.fconn = nn.Sequential(
-            nn.Linear(20*13*13, 20),
+            nn.Linear(30*13*13, 20),
             nn.Sigmoid(),
             nn.Linear(20, 1),
         )
 
     def forward(self, x):
         x = self.conv(x)
-        x = x.view(-1, 20*13*13)
+        x = x.view(-1, 30*13*13)
         x = self.fconn(x)
         return x
